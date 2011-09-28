5aa01d976ee98a544f858d15b262097325c59278
yame <14428812@qq.com>
2011-09-28 12:35:51 +0800

    add test code for beanutils

--- src/main/java/org/yamr/example/App.java
+++ src/main/java/org/yamr/example/App.java
@@ -1,7 +1,34 @@
 package org.yamr.example;
 
+import java.lang.reflect.InvocationTargetException;
+
+import org.apache.commons.beanutils.PropertyUtils;
+import org.yamr.example.bean.Person;
+
 public class App {
 	public static void main(String[] args){
 		System.out.println("Hello,Yame!");
+		
+		Person person = new Person();
+		person.setName("zhengym");
+		
+		
+		try {
+			PropertyUtils.setProperty(person, "name", "Name");
+			String name = (String) PropertyUtils.getProperty(person, "name");
+			PropertyUtils.setProperty(person, "address.street", "LTL build, Nansan,China");
+			String street = person.getAddress().getStreet();
+			System.out.println(street);
+			System.out.println(name);
+		} catch (IllegalAccessException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (InvocationTargetException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (NoSuchMethodException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 }
